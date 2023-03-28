0cf007631430d88a6002b797a75710bbcfe52875
Holandsoest <holandsoest@gmail.com>
2023-03-28 14:10:12 +0200

    made svg exporter

--- source/shapes.py
+++ source/shapes.py
@@ -59,41 +59,34 @@ def save_img(tkinter_canvas:tkinter.Canvas, size_img:loc.Pos, path_filename:str,
         print('WARNING: Could not save as no file format was given.')
         return
     
-    from PIL import Image, ImageDraw
-    import io
-    # import subprocess
-
+    import canvasvg
     tkinter_canvas.pack()
-    ps = tkinter_canvas.postscript(file = path_filename + '.eps', colormode='color') 
+    tkinter_canvas.update()
+    canvasvg.saveall('img.svg', canvas=tkinter_canvas)
 
-    # PIL image can be saved as .png .jpg .gif or .bmp file (among others)
-    img = Image.open(path_filename + '.eps') 
-    if as_png:      img.save(path_filename + '.png', 'png') 
-    if as_jpg:      img.save(path_filename + '.jpg', 'jpg') 
-    if as_gif:      img.save(path_filename + '.gif', 'gif') 
-    if as_bmp:      img.save(path_filename + '.bmp', 'bmp') 
-    if not as_eps:  os.remove(path_filename + '.eps')
+    
+    print ('halt')
 
 
 img_size = loc.Pos(x=200, y=200)
+path = os.path.join(os.getcwd(), 'files', 'shape_generator', 'test_img')
 
 root = tkinter.Tk()
-my_canvas = tkinter.Canvas(root, bg="white", height=img_size.y, width=img_size.x)
+canvas = tkinter.Canvas(root, bg="white", height=img_size.y, width=img_size.x)
 
 
 star1 = Star(center_pos=loc.Pos(x=50, y=50),
              size_in_pixels=24,
              rotation_rad=0.0,
              depth_percentage=50)
-polygon_star1 = my_canvas.create_polygon(star1.get_polygon_coordinates(),
+polygon_star1 = canvas.create_polygon(star1.get_polygon_coordinates(),
                                         outline="blue", width=1,
                                         fill="gray")
 
 
-save_img(tkinter_canvas=my_canvas,
-         path_filename=os.path.join(os.getcwd(), 'files', 'shape_generator', 'test_img'),
+save_img(tkinter_canvas=canvas,
+         path_filename=os.path.join(os.getcwd(), 'files', 'shape_generator'),
          size_img=img_size,
          as_jpg=True)
-
 
 root.mainloop()

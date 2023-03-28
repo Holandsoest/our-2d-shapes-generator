8f89cd4513fd6bdc41bce6fe4e67101ae63c7047
Holandsoest <holandsoest@gmail.com>
2023-03-28 15:23:07 +0200

    jpg broken

--- source/shapes.py
+++ source/shapes.py
@@ -54,18 +54,43 @@ class Star:
             polygon_coordinates.append(  int(round(  node.y,  0  ))  )
         return polygon_coordinates
 
-def save_img(tkinter_canvas:tkinter.Canvas, size_img:loc.Pos, path_filename:str, as_png=False, as_jpg=False, as_gif=False, as_bmp=False, as_eps=False) -> None:
+def save_img(tkinter_canvas:tkinter.Canvas, path_filename:str, as_png=False, as_jpg=False, as_gif=False, as_bmp=False, as_eps=False) -> None:
     if not ( as_png or as_jpg or as_gif or as_bmp or as_eps ):
         print('WARNING: Could not save as no file format was given.')
         return
     
-    import canvasvg
+
     tkinter_canvas.pack()
     tkinter_canvas.update()
-    canvasvg.saveall('img.svg', canvas=tkinter_canvas)
 
-    
-    print ('halt')
+
+    from PIL import Image, ImageTk, EpsImagePlugin
+    if as_eps:
+        canvas.postscript(file = path_filename + '.eps')
+        img = Image.open(path_filename + '.eps')
+    else:
+        import io
+        postscript = canvas.postscript(colormode='color')
+        img = Image.open(io.BytesIO(postscript.encode('utf-8')))
+
+
+    # Warning this requires Ghostscript that has to be installed manually on your operating system
+    #
+    # Error: `OSError: Unable to locate Ghostscript on paths`
+    #
+    # Official guide: https://ghostscript.com/docs/9.54.0/Install.htm
+    #
+    # My guide:
+    # 1. CRY
+    # 2. https://ghostscript.com/releases/gsdnld.html Just brrrr install this as x64
+    # 3. This took me hours :'(
+    EpsImagePlugin.gs_windows_binary =  r'C:\Program Files\gs\gs10.01.1\bin\gswin64c' # This is the default location, Telling PIL that it should be here
+
+
+    if as_jpg: img.save(path_filename + '.jpg', 'jpg')
+    if as_png: img.save(path_filename + '.png', 'png')
+    if as_gif: img.save(path_filename + '.gif', 'gif')
+    if as_bmp: img.save(path_filename + '.bmp', 'bmp')
 
 
 img_size = loc.Pos(x=200, y=200)
@@ -79,14 +104,19 @@ star1 = Star(center_pos=loc.Pos(x=50, y=50),
              size_in_pixels=24,
              rotation_rad=0.0,
              depth_percentage=50)
-polygon_star1 = canvas.create_polygon(star1.get_polygon_coordinates(),
+star2 = Star(center_pos=loc.Pos(x=150, y=100),
+             size_in_pixels=60,
+             rotation_rad=1.0,
+             depth_percentage=10)
+
+canvas.create_polygon(star1.get_polygon_coordinates(),
                                         outline="blue", width=1,
                                         fill="gray")
+# canvas.create_polygon(star2.get_polygon_coordinates(),
+#                                         outline="blue", width=1,
+#                                         fill="gray")
 
 
 save_img(tkinter_canvas=canvas,
          path_filename=os.path.join(os.getcwd(), 'files', 'shape_generator'),
-         size_img=img_size,
-         as_jpg=True)
-
-root.mainloop()
+         as_jpg=True, as_bmp=True, as_gif=True, as_png=True, as_eps=True)
